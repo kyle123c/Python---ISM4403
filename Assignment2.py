4293c7c1010d1a34bd057010ab9def5afd1032bc
KYLE <kyle123c@hotmail.com>
2021-02-03 11:43:15 -0500

    Update Assignment2.py

--- Assignment2.py
+++ Assignment2.py
@@ -23,27 +23,53 @@ for x in mylist:
 y = True
 bandlist = []
 while y:
-	userlist2 = input("Enter your favorite bands until they enter 'The show is over' ")
+	userlist2 = input("Enter your favorite bands until they enter 'The shows over' ")
 	print("The Band entered was: " +str(userlist2))
-	if userlist2 == "The show is over":
-		print("The input was 'The show is over' ...Exiting...")
+	if userlist2 == "The shows over":
+		print("The input was 'The shows over' ...Exiting...")
 		y = False
 	else:
 		bandlist.append(userlist2)
 		continue
-	gametuple = ("MK",)			
-	bandtuple = ("Kiss",)
-	gamebandtuple = gametuple + bandtuple
-	print(gamebandtuple)					#4
-	'''
-		for x in mylist:
-		if x in gametuple:
-			print("That is a great a game")
-		if x in bandtuple:
-			print("Thats is my favorite band!")
-		if x in mylist + bandlist:
-			print("That is the perfect combination!")	
+	print(" Band list: " , bandlist)
 
+#4	
+myfavtuple = ("MK","KISS")
 
-'''
+#5
 
+gcounter = 0
+
+bcounter = 0
+
+for x in myfavtuple:
+    for y in mylist:
+        if x in y:
+            gcounter += 1
+    for z in bandlist:
+        if x in z:
+            bcounter += 1
+
+if gcounter >= 1 and bcounter < 1:
+    print("Thats a great game")
+
+if gcounter < 1 and bcounter >= 1:
+    print("I love that band")
+
+if gcounter >= 1 and bcounter >= 1:
+    print("Thats the perfect combo!")
+
+#6
+
+favorite_games_set = {"Legend of Zelda", "God of War", "Super Smash Bros Ultimate"}
+
+#7
+
+print("My favorite games are: ", favorite_games_set)
+
+#8
+total_set = list(favorite_games_set) + mylist
+
+#9
+
+print("Our best games in no particular order would be : ", total_set)
